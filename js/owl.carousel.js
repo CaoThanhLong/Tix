ea55403c999518c4856242f51a7762a331309442
CaoThanhLong <ctlong26051993@gmail.com>
2020-08-23 22:48:32 +0700

    Hoàn thành html và style cho phần filmblock (Home)

---
+++ js/owl.carousel.js
@@ -0,0 +1,25 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: true,
+  dot: false,
+  mouseDrag: false,
+  touchDrag: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    576: {
+      items: 1,
+    },
+    768: {
+      items: 1,
+    },
+    992: {
+      items: 1,
+    },
+    1200: {
+      items: 1,
+    },
+  },
+});
